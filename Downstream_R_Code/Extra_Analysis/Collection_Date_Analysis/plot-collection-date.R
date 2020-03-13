8e8c348055baeb210e84ccb1156284a89800f009
Charles Warden <cwarden45@gmail.com>
2020-03-13 16:23:17 -0700

    Add files via upload

--- Downstream_R_Code/Extra_Analysis/Collection_Date_Analysis/plot-collection-date.R
+++ Downstream_R_Code/Extra_Analysis/Collection_Date_Analysis/plot-collection-date.R
@@ -1,6 +1,6 @@
 count.file = "../../Public_Input_Files/PE_HPVtype_counts_final_names.txt"
 #NOTE: genotype column not used, so it doesn't really matter which extended meta table is used
-meta.file = "../../Selected_Output_Files/combined_genotype_with_year_and_ethnicity_freq15.txt"
+meta.file = "../../Selected_Output_Files/combined_genotype_with_year_and_ethnicity_freq20.txt"
 
 count.to.ab = function(counts, total){
 	return(100*(counts/total))
@@ -32,7 +32,7 @@ selected.HPV = c("HPV16","HPV18","HPV58")
 
 min.year = min(meta.table$collection.year, na.rm=T)
 max.year = max(meta.table$collection.year, na.rm=T)
-pdf("HPV_genotype_by_Collection_Date.pdf", width=30, height=10, useDingbats=FALSE)
+png("HPV_genotype_by_Collection_Date.png", width=600, height=200)
 par(mfcol=c(1,3))
 for (i in 1:length(selected.HPV)){
 	plot.type = selected.HPV[i]
@@ -44,19 +44,19 @@ for (i in 1:length(selected.HPV)){
 		pvalue = result$coefficients[2,4]
 		print(paste("r= ",round(cor.coef,digits=2),", lm p-value = ",signif(pvalue, digits=2),sep=""))
 
-	par(mar=c(15,10,7,7))
-	plot(meta.table$collection.year,subtype.freq,cex.main=3, cex.axis=3,cex.lab=3,
-		ylab="", ylim=c(0,100), cex = 2, xlab="", bg=labelColors,
+	par(mar=c(5,8,5,2))
+	plot(meta.table$collection.year,subtype.freq,cex.main=1, cex.axis=1,cex.lab=1,
+		ylab="", ylim=c(0,100), cex = 1, xlab="", bg=labelColors,
 		col="black", pch=21, las=2, xlim=c(min.year,max.year))
 	frozen.fit = lm(subtype.freq[meta.table$batch == "Frozen"]~ meta.table$collection.year[meta.table$batch == "Frozen"])
 	abline(frozen.fit, col="orange")
 	frozen.fit = lm(subtype.freq[meta.table$batch == "FFPE"]~ meta.table$collection.year[meta.table$batch == "FFPE"])
 	abline(frozen.fit, col="cyan")
 	
-	text(1985,112, labels=plot.type, xpd=T, cex=3, font=2)
+	text(1985,120, labels=plot.type, xpd=T, cex=1.2, font=2)
 	mtext("Collection Year",1, cex=2, padj=6)
-	mtext(paste("Percent ",plot.type," Reads",sep=""),2, cex=2, padj=-4)
-	legend(2005,115, legend = c("Frozen","FFPE"), col="black", pt.bg =c("orange","cyan"),
-			ncol=2, pch=21,xpd=T, cex=2, inset=-0.1)
+	mtext(paste("Percent ",plot.type," Reads",sep=""),2, cex=1, padj=-5)
+	legend(1995,125, legend = c("Frozen","FFPE"), col="black", pt.bg =c("orange","cyan"),
+			ncol=2, pch=21,xpd=T, cex=0.8, inset=-0.1)
 }#end for (i in 1:nrow(ab.table))
 dev.off()
