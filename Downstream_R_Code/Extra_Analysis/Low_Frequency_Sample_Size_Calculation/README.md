1a9fb22d73a5606352331170194bbdcbab87a301
Charles Warden <cwarden45@gmail.com>
2020-03-25 11:02:06 -0700

    Add files via upload

---
+++ Downstream_R_Code/Extra_Analysis/Low_Frequency_Sample_Size_Calculation/README.md
@@ -0,0 +1,21 @@
+
+There were 28 archived DNA, 40 frozen tissue, and 43 FFPE ICC samples passing the qCPR filter that we propose (a subset of which would be HPV+ for the genotype analysis).
+
+So, while it doesn't exactly match any of the numbers for the comparisons, p-values for a Fisher Exact test were tested for the following conditions:
+
+**1)** 15% genotype (larger group) versus 5% genotype (smaller group), represented by a *black* line
+**2)** 10% genotype (larger group) versus 5% genotype (smaller group), represented by a *blue* line
+
+The sample size for the larger groups started at 40 samples and the sample size for the smaller group started at 30 samples.
+
+Sample size was incremented by 1 sample per group.  This makes the group size more similar at larger numbers.  The decrements in samples size are not plotted, since the difference in group sizes would become greater (and the goal is to give some sense of how many combined samples would be needed to get an unadjusted p-value < 0.05).
+
+This results in the following plot:
+
+![Sample size estimation](40-30_sample_size_estimation.png "Sample size estimation")
+
+There other methods for p-value calculation that can be more sensitive, and multiple tests were compared in subfolders including but not limited to  [Sample_Type_Full_Statistical_Analysis](https://github.com/cwarden45/HPV_genotype_paper-archived_samples/tree/master/Downstream_R_Code/Extra_Analysis/Sample_Type_Full_Statistical_Analysis).  However, higher false positives (from an underestimated false positive rate) and potentially more samples from the same patient than expected for FFPE samples may also be worth taking into consideration.  So, with the right parameters / filtering, we thought the Fisher's Exact test provided the best balance for our study (as far as we could tell).
+
+The use of intergers can cause the p-values to not strictly decrease, which is why a loess trendline was added.
+
+The dashed lined represent the minimum sample size with a p-value > 0.05 (for individual lines, not the loess fit).
---
+++ Downstream_R_Code/Extra_Analysis/Low_Frequency_Sample_Size_Calculation/hypothetical_sample_size_calculation.R
@@ -0,0 +1,77 @@
+total.max.add = 350
+
+png("40-30_sample_size_estimation.png")
+#15% versus 5%
+print("15% versus 5%")
+large.start = 40
+small.start = 30
+
+total.size = c()
+pvalues = c()
+
+for (i in 0:total.max.add){
+	test.large = large.start + i
+	test.small = small.start + i
+	
+	integer.pos.large = as.integer(0.15 * test.large)
+	integer.pos.small = as.integer(0.05 * test.small)
+	
+	#2-sided, since you don't know which is larger ahead of time
+	fisher.mat = matrix(c(integer.pos.large, test.large, integer.pos.small, test.small), ncol=2)
+	rownames(fisher.mat) = c("pos","neg")
+	colnames(fisher.mat) = c("higher","lower")
+	
+	result = fisher.test(fisher.mat)
+	
+	total.size = c(total.size, test.large + test.small)
+	pvalues = c(pvalues, result$p.value) 
+}#end for (i in 0:total.max.add)
+
+plot(total.size, pvalues,
+	ylim=c(0,1), xlim=c(0,max(total.size) + 5),
+	col=rgb(red=0, green=0, blue=0, alpha=0.5), pch=16)
+max.not.detected = max(total.size[pvalues > 0.05])
+print(max.not.detected)
+abline(v=max.not.detected, col = rgb(red=0, green=0, blue=0, alpha=0.8), lty=4)
+abline(h=0.05)
+loess.model = loess(pvalues ~ total.size)
+lines(total.size, predict(loess.model), col = "black", lwd=4)
+rect(-10, -0.1, max(total.size) + 100, 0.05, col=rgb(red=0, green=1, blue=0, alpha=0.2), border=NA)
+legend("topright", c("15%-vs-5%","10%-vs-5%"),
+		col=c("black","blue"), lwd=4)
+		
+#10% versus 5%
+print("10% versus 5%")
+large.start = 40
+small.start = 30
+
+total.size = c()
+pvalues = c()
+
+for (i in 0:total.max.add){
+	test.large = large.start + i
+	test.small = small.start + i
+	
+	integer.pos.large = as.integer(0.10 * test.large)
+	integer.pos.small = as.integer(0.05 * test.small)
+	
+	#2-sided, since you don't know which is larger ahead of time
+	fisher.mat = matrix(c(integer.pos.large, test.large, integer.pos.small, test.small), ncol=2)
+	rownames(fisher.mat) = c("pos","neg")
+	colnames(fisher.mat) = c("higher","lower")
+	
+	result = fisher.test(fisher.mat)
+	
+	total.size = c(total.size, test.large + test.small)
+	pvalues = c(pvalues, result$p.value) 
+}#end for (i in 0:total.max.add)
+
+points(total.size, pvalues,
+	col=rgb(red=0, green=0, blue=1, alpha=0.5), pch=16)
+max.not.detected = max(total.size[pvalues > 0.05])
+print(max.not.detected)
+abline(v=max.not.detected, col = rgb(red=0, green=1, blue=0, alpha=0.8), lty=4)
+loess.model = loess(pvalues ~ total.size)
+lines(total.size, predict(loess.model), col = "blue", lwd=4)
+rect(-10, -0.1, max(total.size) + 100, 0.05, col=rgb(red=0, green=1, blue=0, alpha=0.2), border=NA)
+dev.off()
