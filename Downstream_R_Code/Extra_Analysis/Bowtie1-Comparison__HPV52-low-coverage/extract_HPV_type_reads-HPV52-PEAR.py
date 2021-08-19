9209e49c70d7e14f0c072aeb89352e283a16f016
Charles Warden <cwarden45@gmail.com>
2021-08-19 09:24:54 -0700

    Add files via upload

---
+++ Downstream_R_Code/Extra_Analysis/Bowtie1-Comparison__HPV52-low-coverage/extract_HPV_type_reads-HPV52-PEAR.py
@@ -0,0 +1,119 @@
+import sys
+import re
+import os
+import subprocess
+
+#NOTE: this requires use of a PEAR-Merged alignment (rather than the paired-end alignment, used for most of the analysis)
+
+#NOTE: this is used to create "HPV16_seq_stats_freq5.txt", "HPV18_seq_stats_freq5.txt", and "HPV58_seq_stats_freq5.txt" in "Downstream_R_Code/"
+
+HPVtype = "HPV52"
+summaryFile = "HPV52_custom_input.txt"
+output_suffix = "_ANY-READS.txt"
+
+minFreq = 15
+
+alignmentFolder = "../../../../../../Code/Round20/PEAR-hg38_plus_35HPV_Alignment-Full_Reads_Combined"#these files are large and not on GitHub (but the output of this script is provided)
+
+seqStats = HPVtype + "_seq_stats" + output_suffix
+outHandle = open(seqStats,"w")
+text = "Sample\tRep.Seq\tRep.Percent\n"
+outHandle.write(text)
+	
+inHandle = open(summaryFile)
+line = inHandle.readline()
+
+lineCount = 0
+
+while line:
+	line = re.sub("\r","",line)
+	line = re.sub("\n","",line)
+	
+	lineCount += 1
+	
+	if lineCount > 1:
+		lineInfo = line.split("\t")
+		HPVresult = re.search(HPVtype,lineInfo[4])
+		sampleID = lineInfo[0]
+		
+		if HPVresult:
+			print HPVtype + " detected in " + sampleID
+			text = sampleID
+			
+			bamFile = alignmentFolder + "/" + sampleID + ".bam"
+			outputfolder = alignmentFolder + "/" + sampleID
+			
+			tempSam = "HPV52/"+sampleID+".sam"
+			
+			outFA = "HPV52/"+HPVtype+"_"+sampleID + ".fa"
+			
+			command = "/opt/samtools-1.3/samtools view " + bamFile + " > " + tempSam
+			os.system(command)
+			
+			inHandle2 = open(tempSam)
+			line2 = inHandle2.readline()
+			
+			faHandle = open(outFA,"w")
+			
+			readHash = {}
+			
+			while line2:
+				line2 = re.sub("\r","",line2)
+				line2 = re.sub("\n","",line2)
+				lineInfo2 = line2.split("\t")
+				chr = lineInfo2[2]
+				if chr == HPVtype:
+					read = lineInfo2[0]
+					seq = lineInfo2[9]
+					
+					if seq in readHash:
+						readHash[seq] = readHash[seq] + 1
+					else:
+						readHash[seq] = 1
+						
+					faHandle.write(">"+read + "\n" + seq + "\n")
+				line2 = inHandle2.readline()
+			
+			inHandle2.close()
+			faHandle.close()
+			
+			revGenoHash = {}
+			typeCount = 0
+				
+			for seq in readHash:
+				seqCount = readHash[seq]
+				typeCount += seqCount
+
+			revSeqHash = {}
+			for seq in readHash:
+				seqCount = readHash[seq]
+				percentSeq = 100* float(seqCount)/float(typeCount)
+				if percentSeq >= minFreq:
+					revSeqHash[percentSeq]=seq
+
+			#order by frequency
+			seqAb = revSeqHash.keys()
+			seqAb.sort(reverse=True)
+					
+			seqText = ""
+			freqText = ""
+			
+			for i in xrange(0,len(seqAb)):
+				freq = seqAb[i]
+				seq = revSeqHash[seqAb[i]]
+				
+				if i == 0:
+					seqText=seq
+					freqText='{0:.2f}'.format(freq)
+				else:
+					seqText=seqText + ", " + seq
+					freqText=freqText + ", " + '{0:.2f}'.format(freq)					
+					
+			text = text + "\t" + seqText + "\t" + freqText + "\n"
+			outHandle.write(text)
+			
+			#command = "rm " + tempSam
+			#os.system(command)
+	line = inHandle.readline()
+
+inHandle.close()
